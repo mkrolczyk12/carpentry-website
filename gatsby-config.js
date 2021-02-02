79c51e7af65649cc2890038c1f98f0541a526b74
mkrolczyk12 <m.krolczyk66@gmail.com>
2021-02-02 15:51:14 +0100

    added .env config for production build mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 require('dotenv').config()
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
     title: "ZagorowskiuslugiStolarskie",
