73c4d968dd204839270844cb79e478f087c0f759
mkrolczyk12 <m.krolczyk66@gmail.com>
2021-02-26 22:15:07 +0100

    added styles for all display in offer and gallery pages

--- src/pages/components/offerGallery/OfferGallery.js
+++ src/pages/components/offerGallery/OfferGallery.js
@@ -13,35 +13,37 @@ const OfferGallery = ({productData = [], redirection = false}) => {
 
     return(
         <section className="offer-gallery">
-            {redirection ?
-                productData.map((each, index) => (     // gallery
-                <Link
-                    to={`/galeria/${each.permalink}`}
-                    className="offer-gallery__item"
-                    key={index}
-                    title="Kliknij aby zobaczyć opis produktu"
-                >
-                    <Img className="offer-gallery__item-image"
-                         fluid={each.imageGallery[0].fluid}
-                         title="Kliknij aby zobaczyć opis produktu"
-                    />
-                    <p className="offer-gallery__item-title" title="Kliknij aby zobaczyć opis produktu">{each.title}</p>
-                </Link>
-            ))
-            :
-                productData.map((each, index) => (     // offer
-                    <div className="offer-gallery__item"
-                         onClick={handleOfferClick}
-                         key={index}
+            <div className="offer-gallery__wrapper">
+                {redirection ?
+                    productData.map((each, index) => (     // gallery
+                    <Link
+                        to={`/galeria/${each.permalink}`}
+                        className="offer-gallery__item"
+                        key={index}
+                        title="Kliknij aby zobaczyć opis produktu"
                     >
-                        <Img className="offer-gallery__item-image" fluid={each.image.fluid} />
-                        <p className="offer-gallery__item-title">{each.title}</p>
-                        <div
-                            className="offer-gallery__item--opening"
-                            dangerouslySetInnerHTML={{__html: each.description}}
+                        <Img className="offer-gallery__item-image"
+                             fluid={each.imageGallery[0].fluid}
+                             title="Kliknij aby zobaczyć opis produktu"
                         />
-                    </div>
-            ))}
+                        <p className="offer-gallery__item-title" title="Kliknij aby zobaczyć opis produktu">{each.title}</p>
+                    </Link>
+                ))
+                :
+                    productData.map((each, index) => (     // offer
+                        <div className="offer-gallery__item"
+                             onClick={handleOfferClick}
+                             key={index}
+                        >
+                            <Img className="offer-gallery__item-image" fluid={each.image.fluid} />
+                            <p className="offer-gallery__item-title">{each.title}</p>
+                            <div
+                                className="offer-gallery__item--opening"
+                                dangerouslySetInnerHTML={{__html: each.description}}
+                            />
+                        </div>
+                ))}
+            </div>
         </section>
     )
 }
